5e7cd7ca6fed9f9113149d908233f21ef4006ab8
benswift404 <swiftmac234@gmail.com>
2017-01-01 18:40:40 -0500

    yes

---
+++ benswift_new/assets/js/form.js
@@ -0,0 +1,21 @@
+$(function($) {
+   $('#contact-form').submit(function(e) {
+       var name = document.getElementById('name');
+       var email = document.getElementById('email');
+       var message = document.getElementById('message');
+       
+       if (!name.value || !email.value || !message.value) {
+           alertify.error("Make sure your form is complete!")
+       } else {
+           $.ajax({
+                url: "https://formspree.io/benswift404@gmail.com", 
+                method: "POST",
+                data: $(this).serialize(),
+                dataType: "json"
+            });
+           e.preventDefault();
+           $(this).get(0).reset()
+           alertify.success("Message sent!");
+       }
+   }); 
+});
